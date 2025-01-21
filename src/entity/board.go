2ae16f6ddbddeaaa8dfd6c3cb5a639cc9165d365
Alex Zhu <alex@alexazhu.com>
2025-01-21 05:53:23 -0800

    Return tiles in sorted order by value

--- src/entity/board.go
+++ src/entity/board.go
@@ -1,5 +1,10 @@
 package entity
 
+import (
+	"maps"
+	"slices"
+)
+
 const (
 	BoardSize    = 5
 	MaxWildcards = 1
@@ -9,9 +14,32 @@ type Board struct {
 	Tiles       map[rune]Tile
 	Multipliers [][]int // Grid of multipliers
 	BonusWord   [][]int // Slice of [row,col] coords
+
+	sortedTiles []rune
 }
 
 type Tile struct {
 	Value int
 	Count int
+}
+
+func Less(a, b Tile) bool {
+	return a.Value < b.Value
+}
+
+func (b Board) SortedTiles() []rune {
+	if b.sortedTiles != nil {
+		return b.sortedTiles
+	}
+
+	tiles := make([]rune, 0, len(b.Tiles))
+	for letter := range maps.Keys(b.Tiles) {
+		tiles = append(tiles, letter)
+	}
+	slices.SortFunc(tiles, func(x, y rune) int {
+		return b.Tiles[y].Value - b.Tiles[x].Value
+	})
+
+	b.sortedTiles = tiles
+	return tiles
 }
